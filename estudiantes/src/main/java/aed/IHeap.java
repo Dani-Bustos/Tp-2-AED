d594aa7f18b0f19750efb9cb57fb2b03c30e00c0
maromalo <54760464+maromalo@users.noreply.github.com>
2023-11-03 15:19:01 -0300

    Update IHeap.java

--- estudiantes/src/main/java/aed/IHeap.java
+++ estudiantes/src/main/java/aed/IHeap.java
@@ -1,7 +1,8 @@
 package aed;
 
 public interface IHeap {
-    public int proximo(); //O(1)
+    public int cardinal(); //O(1)
+    public int maximo(); //O(1)
     public void encolar(int elem); //O(log n)
     public void desencolar(); //O(log n)
     public int segundoMaximo(); //O(1)
