b8a4a05562f9e8ed1903707682a5eb2e245fd211
a <clinux01@pc38.lab1107>
2023-11-03 12:33:26 -0300

    bb

--- estudiantes/src/main/java/aed/SistemaCNE.java
+++ estudiantes/src/main/java/aed/SistemaCNE.java
@@ -1,6 +1,39 @@
 package aed;
 public class SistemaCNE {
-    // Completar atributos privados
+    private int[] vPresidente;
+    private String[] nomPartido;
+    private int[] bancasXDistrito;
+    private int[] mesas; //guardamos el ultimo elemento de la mesa, no incluido
+    private int[][] votosXDistrito; //Primer Array distrito, segundo array votos de cada partido 
+    // private array de heaps, cada pos es un distrito
+
+    private int BusquedaBinariaEnRango(int[] arreglo,int elem){
+        int l = 0;
+        int r = arreglo.length;
+        int m = (l + r)/2;
+        while(l != r){
+           if(arreglo[m] == elem){
+            return m;
+           }else if(r-l == 1 && elem != r && elem != l){ //caso en el que no pertenezeca a la lista directamente
+            if(elem < arreglo[l]){
+            return l;
+            } 
+            else{
+            return r;
+            }
+           }else if(arreglo[m] > elem){
+            r = m;
+            
+           }else if (arreglo[m] < elem){
+            l = m;
+            
+           }
+            m = (r + l)/2;    
+        }
+        return m;
+    }
+    
+    
 
     public class VotosPartido{
         private int presidente;
