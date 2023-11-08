36ac899019c9d26368a54835c1834b8d57ebce82
Dani-Bustos <clinux01@pc22.lab1107>
2023-11-08 16:05:27 -0300

    Complejidades

--- estudiantes/src/main/java/aed/PriorityQueueTupla.java
+++ estudiantes/src/main/java/aed/PriorityQueueTupla.java
@@ -5,55 +5,76 @@ import aed.SistemaCNE.PartidoXVoto;
 public class PriorityQueueTupla implements IHeap<PartidoXVoto> {
     //Inv rep
     // La longitud del arreglo representacion es siempre igual al tamaño 
-    // A PREGUNTAR , SI TENEMOS QUE REAJUSTAR EL ARREGLO PARA QUE SEA UN INVARIANTE MAS "PROPIO"
+    // El primer elemento de representacion es el mayor, se cumple el invariante del heap en la representacion
+    // Es decir: El ultimo nivel esta "lleno" desde la izquierda, es izquierdista. 
+    // Valiendo que todos los padres son mayores o iguales que sus hijos, si los tiene.
+    // Si una posicion de representacion es null, todas las que le siguen seran tambien null
+    // La representacion es un arbol binario perfectamente balanceado, implementado sobre arreglo
     private int tamaño;
     private PartidoXVoto[] representacion;
-
+    
+    //Complejidad O(1)
     public PriorityQueueTupla(int buffer) {
         tamaño = 0;
         representacion = new PartidoXVoto[buffer];
     }
-
+   
+    //Complejidad : O(n), siendo n la longitud del arreglo. 
     public PriorityQueueTupla(PartidoXVoto[] arreglo) { 
         tamaño = arreglo.length;
+        //Evitamos aliasing
         representacion = new PartidoXVoto[tamaño];
         for (int i = 0; i < arreglo.length; i++) {
             representacion[i] = arreglo[i];
         }
-        for (int h = altura() - 1; h > 0; h--) { //algoritmo de Floyd
+        //Observar que la altua es menor o igual a la longitud del arreglo
+        for (int h = altura() - 1; h > 0; h--) { //Algoritmo de Floyd para Array2heap
             for (int i = (int)Math.pow(2, h-1) - 1; i < (int)Math.pow(2, h) - 1; i++) {
                 bajar(i);
             }
         }
     }
 
+    // Complejidad : O(n)
     public int altura() {
         int altura = 0;
         for (int i = 0; i < tamaño; i = i * 2 + 1) altura++;
         return altura;
     }
-
+     
+    // Complejidad : O(1)
     public int cardinal() { return tamaño; }
-
+    
+    // Complejidad : O(1), indexar arreglo
     public PartidoXVoto maximo() { return representacion[0]; }
-
+    
+    // Complejidad : O(1), indexar arreglo y comparacion
     public PartidoXVoto segundoMaximo() { 
         if (tamaño == 2) return representacion[1];
-        else return ((representacion[1].compareTo(representacion[2]) > 0) ? representacion[1] : representacion[2]);  
+        else {if (representacion[1].compareTo(representacion[2]) > 0){  
+            return  representacion[1]; 
+            } 
+            else {  
+                return representacion[2];
+            }  
+        } 
     }
-
+    
+    //Complejidad O(log(n))
     public void encolar(PartidoXVoto elem) {
+        
         representacion[tamaño] = elem;
         int ielem = tamaño;
         tamaño += 1;
         int ipadre = (ielem - 1) / 2;
+        // Lo maximo que podemos "trepar" por el arbol es su altura esta siendo log(n), con n la longitud de la representacion
         while (representacion[ielem].compareTo(representacion[ipadre]) > 0) {
             intercambiar(ielem, ipadre);
             ielem = ipadre;
             ipadre = (ielem - 1) / 2;
         }
     }
-
+    //Complejidad O(log(n))
     public PartidoXVoto desencolar() {
         PartidoXVoto res;
         if(tamaño == 0){
@@ -63,12 +84,16 @@ public class PriorityQueueTupla implements IHeap<PartidoXVoto> {
         representacion[0] = representacion[tamaño - 1];
         representacion[tamaño-1] = null;
         tamaño -= 1;
+        //O(log(n))
         bajar(0);
         }
         return res;
     }
-
+    
+    //Complejidad O(log(n))
     private void bajar(int raiz) {
+        //O(log(n)) , lo maximo que podemos "bajar" en el arbol desde la raiz es su altura
+        // esta siendo log(n), con n el tamaño de la representacion
         int ihijo = iHijoMayor(raiz);
         while (representacion[ihijo] != null && representacion[raiz].compareTo(representacion[ihijo]) < 0) {
             intercambiar(raiz, ihijo);
@@ -76,14 +101,18 @@ public class PriorityQueueTupla implements IHeap<PartidoXVoto> {
             ihijo = iHijoMayor(ihijo);
         }
     }
-
+    
+      //Complejidad O(1)
     private void intercambiar(int i, int j) {
+        //O(1), indexacion de arreglo
         PartidoXVoto temp = representacion[i];
         representacion[i] = representacion[j];
         representacion[j] = temp;
     }
-
+    
+    //Complejidad O(1)
     private int iHijoMayor(int i) {
+        //O(1), comparacion e indexacion de arreglo
         int iHijoIzq = i * 2 + 1;
         if (iHijoIzq >= tamaño) return i; //Como los casos son excluyentes, retornamos antes
         int iHijoDer = i * 2 + 2;
--- estudiantes/src/main/java/aed/SistemaCNE.java
+++ estudiantes/src/main/java/aed/SistemaCNE.java
@@ -16,13 +16,17 @@ public class SistemaCNE {
            // Inv Rep
            // Tanto id partido como votos son numeros positivos o 0.
            
+           //Complejidad O(1)
            PartidoXVoto(int numPartido, int votos){
                 this.idPartido = numPartido;
                 this.Votos = votos;
             }
         
+             //Complejidad O(1), comparacion
             @Override
             public int compareTo(PartidoXVoto a){
+                //Definimos criterio de comparación en las tuplas como la comparación numerica de sus 1eros elementos
+                //para poder asi utilizarla en el heap
                 if(this.Votos > a.Votos){
                     return 1;
                 }else if(this.Votos < a.Votos){
@@ -47,30 +51,37 @@ public class SistemaCNE {
         // El total es la suma de todos los elementos de _arrPresidencial, es decir, el total de votos.
         // _arrPresidencial es un arreglo en el cual en la posicion i-esima se encuentra la cantidad de votos para presidente que obtuvo
         // el i-esimo partido. En la ultima posicion del arreglo se encuentra la cantidad de votos en blanco. Todos los votos son numeros positivos o 0.
+        
+        //Complejidad Theta(n), siendo n el parametro "longitud"
         Presidencial(int longitud){
             this._arrPresidente = new int[longitud];
             _Total = 0;
             idPrimero = 0;
             idSegundo = 1;
         }
-       
+        
+        //Complejidad O(1), indexar arreglo
         int get(int id){
             return _arrPresidente[id];
         }
+        
+        //Complejidad O(1), indexar arreglo
         int max(){
          return _arrPresidente[idPrimero];
        } 
-
+        
+       //Complejidad O(1), indexar arreglo
         int sdoMax(){
             return _arrPresidente[idSegundo];
         }
-       
+      
+        //Complejdad O(1), retornar int
        int total(){
             return _Total;
         }
-       
+        //Complejidad O(P),
         void actualizar(int[] votos){
-        
+        // O(P), se recorre e indexa el arreglo
         for(int i = 0;i < votos.length;i++){
             _Total += votos[i];
             _arrPresidente[i] += votos[i];
@@ -81,7 +92,7 @@ public class SistemaCNE {
                 idSegundo = idPrimero;
                 idPrimero = i;
            }
-           //debemos utiliza Mayor igual que el primero porque sino puede ocurrir que el primero sea le mismo partido que el segundo
+           //debemos utiliza Mayor igual que el primero porque sino puede ocurrir que el primero sea el mismo partido que el segundo
            else if (_arrPresidente[i] > _arrPresidente[idSegundo] && !(_arrPresidente[i] >= _arrPresidente[idPrimero])){
                 idSegundo = i;
            }
@@ -111,37 +122,47 @@ public class SistemaCNE {
         // Es decir, en el heap guardamos los votos de aquellos partidos que pasan el umbral de 3% en su distrito, Y heap valido nos indica si ese heap esta 
         // intacto o no. Si no lo estuviese utilizamos el resultado precalculado iesimo(que corresponde al distrito). 
         
-        
+        //Complejidad de O(D*P)
         Diputados(int longitudDistritos, int longitudPartidos){
-          votosXDistrito = new int[longitudDistritos][longitudPartidos]; //se escribe asi?
+          // Crear un arreglo de longitud n tiene complejidad Theta(n)
+          // Si creamos D arreglos de longitud P, nuestra complejidad es de Theta(D*P)
+          // Las demas creaciones de arreglos no afectan ya que Theta(D + D*P) pertenece a Theta(D*P)
+          
+          votosXDistrito = new int[longitudDistritos][longitudPartidos]; 
           heapValido = new boolean[longitudDistritos];
           heapXDistrito = new PriorityQueueTupla[longitudDistritos];
           resultadosPrecalculados = new int[longitudDistritos][longitudPartidos-1];
           total = new int[longitudDistritos];    
         }
         
+        //O(1), indexacion de arreglos
         int get(int idDistrito, int idPartido){
               return votosXDistrito[idDistrito][idPartido];
         }
-    
+       
+                
+        // Complejidad O(P)
         void actualizarDistrito(int idDistrito,PartidoXVoto[] votosMesa){
             int pasa3PorCiento = 0;
+            // Theta(P), votosMesa tiene longitud de los Partidos
             for(int i = 0; i < votosMesa.length;i++){
                 votosXDistrito[idDistrito][i] += votosMesa[i].Votos;
                 total[idDistrito] += votosMesa[i].Votos;
             }
             
             //Contamos cuantos pasan el 3% para definir la longitud del arreglo que sera el Heap, sin votos en blanco
+            // Theta(P)
             for(int i = 0; i<votosXDistrito[idDistrito].length-1;i++){
-                if(votosXDistrito[idDistrito][i]*100 >= total[idDistrito]*3){ //calculo del 3% ajustado para evitar floating point
+                //calculo del 3% ajustado para evitar floating point
+                if(votosXDistrito[idDistrito][i]*100 >= total[idDistrito]*3){ 
                      pasa3PorCiento ++;
                 }
             }
             
             //Creamos el array con aquellos q pasan el 3 por ciento, que luego sera el heap;
-            //Necesitamso un indice separado para el segundo arreglo,ya que no se corresponden las posiciones con el original
+            //Necesitamos un indice separado para el segundo arreglo, ya que no se corresponden las posiciones con el original
             //a la hora de asignar
-
+            //Theta(P), observar que crear un Nuevo "PartidoXVoto" es O(1), opera como una tupla
             PartidoXVoto[] votosPasanMargen = new PartidoXVoto[pasa3PorCiento];
             int indice_arreglo = 0;
             for(int i = 0; i < votosXDistrito[idDistrito].length-1;i++){
@@ -152,7 +173,8 @@ public class SistemaCNE {
                     indice_arreglo++;
                 }
             }
-            
+            //Internamente el HeapXDistrito utiliza el algoritmo de Floyd cuando se le pasa un arreglo, de complejidad lineal
+            //O(P)
             heapXDistrito[idDistrito] = new PriorityQueueTupla(votosPasanMargen);
            
             heapValido[idDistrito] = true;
@@ -160,10 +182,13 @@ public class SistemaCNE {
         }
        
         int[] devolverBancas(int idDistrito,int cantidadBancas) {
+            // Chequeamos si ya calculamos el resultado de este distrito
             if (heapValido[idDistrito]) {
+                //O(P)
                 int[] bancas = new int[votosXDistrito[0].length - 1];
                 
                 for (int i = 0; i < cantidadBancas; i++) {
+                    //O(log(P)), en el peor caso todos los partidos pasan el margen de 3 por ciento
                     PartidoXVoto max = heapXDistrito[idDistrito].desencolar();
                     if(max != null){
 
@@ -189,90 +214,119 @@ public class SistemaCNE {
     public class VotosPartido{
         private int presidente;
         private int diputados;
+        // Inv Rep:
+        // Tanto presidentes como Diputados son numeros positivos o 0
+        
+        
+        //Complejidad O(1), asignacion de valores 2 veces;
         VotosPartido(int presidente, int diputados)
         {
             this.presidente = presidente; this.diputados = diputados;
         }
+        
+        //Complejidad O(1)
         public int votosPresidente(){return presidente;}
+        
+        //Complejidad O(1)
         public int votosDiputados(){return diputados;}
     }
 
+    //Complejidad O(D*P), siendo D la cantidad de Distritos y P la de Partidos
     public SistemaCNE(String[] nombresDistritos, int[] diputadosPorDistrito, String[] nombresPartidos, int[] ultimasMesasDistritos) {
      nomDistrito = nombresDistritos;
      bancasXDistrito = diputadosPorDistrito;
      nomPartido = nombresPartidos;
      mesas = ultimasMesasDistritos;
-     
+     //O(P) , justificado en la clase
      votosPresidente = new Presidencial(nombresPartidos.length);
+     //O(D*P), justificado en la clase
      votosDiputados = new Diputados(nombresDistritos.length,nombresPartidos.length);
     }
-
+    
+    //Complejidad O(1), indexar arreglo
     public String nombrePartido(int idPartido) {
         return nomPartido[idPartido];
     }
-
+    
+    //Complejidad O(1), indexar arreglo
     public String nombreDistrito(int idDistrito) {
         return nomDistrito[idDistrito];
     }
-
+    
+    //Complejidad O(1), indexar arreglo
     public int diputadosEnDisputa(int idDistrito) {
         return bancasXDistrito[idDistrito];
     }
-
+    
+    //Complejidad O(1), indexar arreglo
     public String distritoDeMesa(int idMesa) {
-        int pos = BusquedaBinariaEnRango(mesas, idMesa);
+        int pos = ObtenerIdDistrito(idMesa);
         return nombreDistrito(pos);
     }
-    private int BusquedaBinariaEnRango(int[] arreglo,int elem){
-        int l = 0;
-        int r = arreglo.length;
-        int m = (l + r)/2;
-        while(l != r){
-           if(arreglo[m] == elem){
-            return m + 1;   // el arreglo es no incluido, con lo cual si lo encontramos, es la siguiente pos de mesa
-           }else if(r-l == 1 && elem != arreglo[r] && elem != arreglo[l]){ //caso en el que no pertenezeca a la lista directamente
-            if(elem < arreglo[l]){
-            return l;
-            } 
-            else{
-            return r;
-            }
-           }else if(arreglo[m] > elem){
-            r = m;
-            
-           }else if (arreglo[m] < elem){
-            l = m;
-            
-           }
-            m = (r + l)/2;    
-        }
-        return m;
-    }
     
-
+    
+    
+    // Complejidad O(P + log(D))
     public void registrarMesa(int idMesa, VotosPartido[] actaMesa) {
         //Actualizamos Votos Presidenciales
+        
+        //Complejidad O(P)
         int []vPres = new int[actaMesa.length];
         for(int i = 0;i < actaMesa.length;i++){
             vPres[i] = i;
             vPres[i] = actaMesa[i].presidente;
         }
+        //Complejidad O(P), justificacion en clase VotosPresidente
         votosPresidente.actualizar(vPres);
        
-        //Ahora Diputados
-        int distritoMesa = BusquedaBinariaEnRango(mesas,idMesa);
+        //Actualizamos Votos Diputados
+        
+        //O(log(D)) Obtener id Distrito usa una busqueda binaria sobre un arreglo ordenado , de longitud distritos
+        int distritoMesa = ObtenerIdDistrito(idMesa);
+        //O(D), creacion de tupla es O(1), realizado tantas veces como la cantidad de distritos
         PartidoXVoto[] vDiputados = new PartidoXVoto[actaMesa.length];
         for (int i = 0; i < actaMesa.length; i++) {
             vDiputados[i] = new PartidoXVoto(i, actaMesa[i].diputados);
             
         }
+        //Complejidad : O(P), justificacion en la clase VotosDiputados
         votosDiputados.actualizarDistrito(distritoMesa,vDiputados);
     }
+ 
+    //Complejidad O(log(n)), siendo  n la longitud del arreglo pasado
+    // Utilizamos una busqueda binaria para obtenerlo, ya que segun el requiere las mesas son siempre un arreglo ordenado.
+    // La busqueda binaria tiene complejidad logaritimica, ya que reduce el rango de busqueda a la mitad en cada iteracion del while
+    private int ObtenerIdDistrito(int elem){
+        int izq = 0;
+        int der = mesas.length;
+        int medio = (izq + der)/2;
+        while(izq != der){
+           if(mesas[medio] == elem){
+            return medio + 1;   // el arreglo es no incluido, con lo cual si lo encontramos, es la siguiente pos de mesa
+           }else if(der - izq == 1 && elem != mesas[der] && elem != mesas[izq]){ //caso en el que no pertenezeca a la lista directamente
+            if(elem < mesas[izq]){
+            return izq;
+            } 
+            else{
+            return der;
+            }
+           }else if(mesas[medio] > elem){
+            der = medio;
+            
+           }else if (mesas[medio] < elem){
+            izq = medio;
+            
+           }
+            medio = (der + izq)/2;    
+        }
+        return medio;
+    }
 
+    //Complejidad O(1)
     public int votosPresidenciales(int idPartido) {
         return votosPresidente.get(idPartido);
     }
-
+    //Complejidad O(1)
     public int votosDiputados(int idPartido, int idDistrito) {
         return votosDiputados.get(idDistrito,idPartido);
     }
