17b29d2915d0042258c3093df19ad551c8714c04
IsidroCouffignal <132113299+IsidroCouffignal@users.noreply.github.com>
2023-11-10 01:06:15 -0300

    Un poquito de belleza en PriorityQueueTupla

--- estudiantes/src/main/java/aed/PriorityQueueTupla.java
+++ estudiantes/src/main/java/aed/PriorityQueueTupla.java
@@ -3,17 +3,19 @@ package aed;
 import aed.SistemaCNE.PartidoXVoto;
 
 public class PriorityQueueTupla implements IHeap<PartidoXVoto> {
-    //Inv rep
-    // La longitud del arreglo representacion es siempre igual al tamaño 
-    // El primer elemento de representacion es el mayor, se cumple el invariante del heap en la representacion
-    // Es decir: El ultimo nivel esta "lleno" desde la izquierda, es izquierdista. 
-    // Valiendo que todos los padres son mayores o iguales que sus hijos, si los tiene.
-    // Si una posicion de representacion es null, todas las que le siguen seran tambien null
-    // La representacion es un arbol binario perfectamente balanceado, implementado sobre arreglo
+
+    //INV REP :
+    // La longitud del arreglo representación es siempre igual a la variable tamaño. 
+    // El primer elemento de representación es el mayor; se cumple el invariante del heap en la representación;
+    // es decir: el último nivel esta "lleno" desde la izquierda, es izquierdista; 
+    // valiendo que todos los padres son mayores o iguales que sus hijos, de tenerlos.
+    // Si una posición de representación es null, todas las que le siguen serán también null.
+    // La representación es un árbol binario perfectamente balanceado, implementado sobre un arreglo.
+
     private int tamaño;
     private PartidoXVoto[] representacion;
     
-    //Complejidad O(1)
+    //Complejidad : O(1)
     public PriorityQueueTupla(int buffer) {
         tamaño = 0;
         representacion = new PartidoXVoto[buffer];
@@ -22,33 +24,33 @@ public class PriorityQueueTupla implements IHeap<PartidoXVoto> {
     //Complejidad : O(n), siendo n la longitud del arreglo. 
     public PriorityQueueTupla(PartidoXVoto[] arreglo) { 
         tamaño = arreglo.length;
-        //Evitamos aliasing
+        //Evitamos aliasing.
         representacion = new PartidoXVoto[tamaño];
         for (int i = 0; i < arreglo.length; i++) {
             representacion[i] = arreglo[i];
         }
-        //Observar que la altura es menor o igual a la longitud del arreglo
-        for (int h = altura() - 1; h > 0; h--) { //Algoritmo de Floyd para Array2heap
+        //Observar que la altura es menor o igual a la longitud del arreglo.
+        for (int h = altura() - 1; h > 0; h--) { //Algoritmo de Floyd para Array2heap.
             for (int i = (int)Math.pow(2, h-1) - 1; i < (int)Math.pow(2, h) - 1; i++) {
                 bajar(i);
             }
         }
     }
 
-    // Complejidad : O(n)
+    //Complejidad : O(n)
     public int altura() {
         int altura = 0;
         for (int i = 0; i < tamaño; i = i * 2 + 1) altura++;
         return altura;
     }
      
-    // Complejidad : O(1)
+    //Complejidad : O(1)
     public int cardinal() { return tamaño; }
     
-    // Complejidad : O(1), indexar arreglo
+    //Complejidad : O(1), indexar arreglo.
     public PartidoXVoto maximo() { return representacion[0]; }
     
-    // Complejidad : O(1), indexar arreglo y comparacion
+    //Complejidad : O(1), indexar arreglo y comparación.
     public PartidoXVoto segundoMaximo() { 
         if (tamaño == 2) return representacion[1];
         else {if (representacion[1].compareTo(representacion[2]) > 0){  
@@ -60,21 +62,21 @@ public class PriorityQueueTupla implements IHeap<PartidoXVoto> {
         } 
     }
     
-    //Complejidad O(log(n))
+    //Complejidad : O(log(n))
     public void encolar(PartidoXVoto elem) {
         
         representacion[tamaño] = elem;
         int ielem = tamaño;
         tamaño += 1;
         int ipadre = (ielem - 1) / 2;
-        // Lo maximo que podemos "trepar" por el arbol es su altura esta siendo log(n), con n la longitud de la representacion
+        // Lo máximo que podemos "trepar" por el árbol es su altura, siendo esta log(n), con n la longitud de la representación.
         while (representacion[ielem].compareTo(representacion[ipadre]) > 0) {
             intercambiar(ielem, ipadre);
             ielem = ipadre;
             ipadre = (ielem - 1) / 2;
         }
     }
-    //Complejidad O(log(n))
+    //Complejidad : O(log(n))
     public PartidoXVoto desencolar() {
         PartidoXVoto res;
         if(tamaño == 0){
@@ -90,10 +92,10 @@ public class PriorityQueueTupla implements IHeap<PartidoXVoto> {
         return res;
     }
     
-    //Complejidad O(log(n))
+    //Complejidad : O(log(n))
     private void bajar(int raiz) {
-        //O(log(n)) , lo maximo que podemos "bajar" en el arbol desde la raiz es su altura
-        // esta siendo log(n), con n el tamaño de la representacion
+        //O(log(n)), lo máximo que podemos "bajar" en el árbol desde la raiz es su altura,
+        //siendo esta log(n), con n el tamaño de la representación.
         int ihijo = iHijoMayor(raiz);
         while (representacion[ihijo] != null && representacion[raiz].compareTo(representacion[ihijo]) < 0) {
             intercambiar(raiz, ihijo);
@@ -102,19 +104,19 @@ public class PriorityQueueTupla implements IHeap<PartidoXVoto> {
         }
     }
     
-      //Complejidad O(1)
+    //Complejidad : O(1)
     private void intercambiar(int i, int j) {
-        //O(1), indexacion de arreglo
+        //O(1), indexación de arreglo.
         PartidoXVoto temp = representacion[i];
         representacion[i] = representacion[j];
         representacion[j] = temp;
     }
     
-    //Complejidad O(1)
+    //Complejidad : O(1)
     private int iHijoMayor(int i) {
-        //O(1), comparacion e indexacion de arreglo
+        //O(1), comparación e indexación de arreglo.
         int iHijoIzq = i * 2 + 1;
-        if (iHijoIzq >= tamaño) return i; //Como los casos son excluyentes, retornamos antes
+        if (iHijoIzq >= tamaño) return i; //Como los casos son excluyentes, retornamos antes.
         int iHijoDer = i * 2 + 2;
         if (iHijoDer >= tamaño) return iHijoIzq;
         else if  (representacion[iHijoDer].compareTo(representacion[iHijoIzq]) > 0)  {return iHijoDer;} else {return iHijoIzq;}
@@ -122,5 +124,3 @@ public class PriorityQueueTupla implements IHeap<PartidoXVoto> {
 
     
 }
-
-
